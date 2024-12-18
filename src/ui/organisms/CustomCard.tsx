bf7b5b82f6fa6880240da5124c68f23a974b90fb
Your Name <you@exmple.com>
2024-12-18 21:13:57 +0700

    more shadcn components

---
+++ src/ui/organisms/CustomCard.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, FC } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface CustomCardProps extends ComponentProps<any> {}
+
+export const CustomCard: FC<CustomCardProps> = (props) => {
+  const { style } = props;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
